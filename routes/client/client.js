4a0b4f6b2805e5fe53db613da51fbc14ea684aa5
Vedanta Somnathe <vedanta1412@gmail.com>
2023-08-09 21:59:57 +0530

    discord

--- routes/client/client.js
+++ routes/client/client.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+    require("dotenv").config();
 }
 // https://medium.com/@dhaniNishant/creating-limit-skip-between-exclude-functions-for-javascript-arrays-4d60a75aaae7
 // https://ourcodeworld.com/articles/read/278/how-to-split-an-array-into-chunks-of-the-same-size-easily-in-javascript
@@ -18,97 +18,99 @@ let blogPage = 1;
 
 // FUNCTIONS
 function limit(c) {
-  return this.filter((x, i) => {
-    if (i <= c - 1) {
-      return true;
-    }
-  });
+    return this.filter((x, i) => {
+        if (i <= c - 1) {
+            return true;
+        }
+    });
 }
 Array.prototype.limit = limit;
 
 function skip(c) {
-  return this.filter((x, i) => {
-    if (i > c - 1) {
-      return true;
-    }
-  });
+    return this.filter((x, i) => {
+        if (i > c - 1) {
+            return true;
+        }
+    });
 }
 Array.prototype.skip = skip;
 
 // ROUTES
 router.get("/", async (req, res) => {
-  let faqs = await Faq.find();
-  res.render("client/index", { faqs: faqs });
+    let faqs = await Faq.find();
+    res.render("client/index", { faqs: faqs });
 });
 
 router.get("/result", (req, res) => {
-  res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
 router.get("/members", async (req, res) => {
-  let members = await Member.find().sort({ pos: 1 });
-  res.render("client/members.ejs", { members: members });
+    let members = await Member.find().sort({ pos: 1 });
+    res.render("client/members.ejs", { members: members });
 });
 
 router.get("/resources", async (req, res) => {
-  let resources = await Resource.find();
-  res.render("client/resources", { resources: resources });
+    let resources = await Resource.find();
+    res.render("client/resources", { resources: resources });
 });
 
 router.get("/archives", async (req, res) => {
-  return res.redirect("/archives/Creative%20Work");
+    return res.redirect("/archives/Creative%20Work");
 });
 
 router.get("/archives/:category", async (req, res) => {
-  let allArchives = await Archive.find({ category: req.params.category }).sort({
-    year: -1,
-  });
+    let allArchives = await Archive.find({
+        category: req.params.category,
+    }).sort({
+        year: -1,
+    });
 
-  if (allArchives.length === 0) {
-    return res.redirect("/404");
-  }
-
-  let allEvents = [];
-  let prEvents = [];
-  let getEvents = await Events.find();
-  for (let event of getEvents) {
-    prEvents.push(event.name);
-  }
-
-  for (let event of prEvents) {
-    for (let archive of allArchives) {
-      if (archive.event == event) {
-        if (!allEvents.includes(event)) allEvents.push(event);
-      }
+    if (allArchives.length === 0) {
+        return res.redirect("/404");
     }
-  }
 
-  let events = allEvents;
-  let obj = {};
-  for (let event of events) {
-    let archivesForComp = await Archive.find({
-      event: event,
-      category: req.params.category,
-    }).sort({ year: -1 });
-    obj[event.toString()] = archivesForComp;
-  }
+    let allEvents = [];
+    let prEvents = [];
+    let getEvents = await Events.find();
+    for (let event of getEvents) {
+        prEvents.push(event.name);
+    }
 
-  res.render("client/archive", {
-    obj: obj,
-    events: events,
-    category: req.params.category,
-  });
+    for (let event of prEvents) {
+        for (let archive of allArchives) {
+            if (archive.event == event) {
+                if (!allEvents.includes(event)) allEvents.push(event);
+            }
+        }
+    }
+
+    let events = allEvents;
+    let obj = {};
+    for (let event of events) {
+        let archivesForComp = await Archive.find({
+            event: event,
+            category: req.params.category,
+        }).sort({ year: -1 });
+        obj[event.toString()] = archivesForComp;
+    }
+
+    res.render("client/archive", {
+        obj: obj,
+        events: events,
+        category: req.params.category,
+    });
 });
 
 router.get("/discord", async (req, res) => {
-  res.redirect("https://discord.gg/XK5TWYKdw9");
-})
+    res.redirect("https://discord.gg/F68CqdVdjw");
+});
 
 router.get("/guide", async (req, res) => {
-  res.redirect(
-      "https://drive.google.com/file/d/1sr-PidiLxCLoUqJaqSi-IMyg_4IGHT4e/view"
-  );
-})
+    res.redirect(
+        "https://drive.google.com/file/d/1sr-PidiLxCLoUqJaqSi-IMyg_4IGHT4e/view"
+    );
+});
 
 // router.get("/blog/:id", async (req, res) => {
 //   let blog = await Blog.findById(req.params.id);
@@ -178,37 +180,37 @@ router.get("/guide", async (req, res) => {
 // });
 
 router.get("/contacts", async (req, res) => {
-  let contacts = await Contact.find();
-  res.render("client/contacts", { contacts: contacts });
+    let contacts = await Contact.find();
+    res.render("client/contacts", { contacts: contacts });
 });
 router.get("/alumni", async (req, res) => {
-  let years = [];
-  let alumniByYear = {};
-  let alumnis = await Alumni.find().sort({ year: -1 });
-  for (let alumni of alumnis) {
-    if (!years.includes(alumni.year)) {
-      years.push(alumni.year);
+    let years = [];
+    let alumniByYear = {};
+    let alumnis = await Alumni.find().sort({ year: -1 });
+    for (let alumni of alumnis) {
+        if (!years.includes(alumni.year)) {
+            years.push(alumni.year);
+        }
     }
-  }
 
-  for (let year of years) {
-    alumniByYear[year] = await Alumni.find({ year: year });
-  }
+    for (let year of years) {
+        alumniByYear[year] = await Alumni.find({ year: year });
+    }
 
-  let sortedKeys = Object.keys(alumniByYear).sort((a, b) => b - a);
+    let sortedKeys = Object.keys(alumniByYear).sort((a, b) => b - a);
 
-  res.render("client/alumni", { obj: alumniByYear, keys: sortedKeys });
+    res.render("client/alumni", { obj: alumniByYear, keys: sortedKeys });
 });
 
 router.get("/soBig", async (req, res) => {
-  let resources = await Resource.find();
-  res.render("client/soBig", { resources: resources });
+    let resources = await Resource.find();
+    res.render("client/soBig", { resources: resources });
 });
 
 router.get("/sobigsguide", (req, res) => {
-  res.redirect(
-    "https://docs.google.com/document/d/117Y9rNNSYK41df7b5XsPZLNOcmonNTQtMTuECoTP3m0/edit?usp=sharing"
-  );
+    res.redirect(
+        "https://docs.google.com/document/d/117Y9rNNSYK41df7b5XsPZLNOcmonNTQtMTuECoTP3m0/edit?usp=sharing"
+    );
 });
 
 router.get("/cw22", (req, res) => {
@@ -216,14 +218,14 @@ router.get("/cw22", (req, res) => {
 });
 
 router.get("/soon", (req, res) => {
-  res.render("client/soon");
+    res.render("client/soon");
 });
 
 router.get("/11b31369", (req, res) => {
-  res.render("client/11b31369");
+    res.render("client/11b31369");
 });
 router.get("/11b31369/stop", (req, res) => {
-  res.render("client/11b31369stop");
+    res.render("client/11b31369stop");
 });
 // EXPORT
 module.exports = router;
